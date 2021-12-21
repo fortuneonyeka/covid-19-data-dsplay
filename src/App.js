d3a12fd9b66cdf6c708fe57aad79c0c51f5e3e09
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-12-21 20:09:10 +0100

    implements routing and navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMicrophone, faCog } from '@fortawesome/free-solid-svg-icons';
+import { Navbar } from 'react-bootstrap';
+import { Routes, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Details from './components/Details';
+import HomePage from './components/HomePage';
 
 function App() {
+
   return (
-    <div className="App">
+    <div>
+      <header>
+        <Navbar className='px-4 text-white bg-pink-400 d-flex justify-content-between'>
+          <Link to='/' className='text-decoration-none text-white fw-bold'>
+            &#60; HOME
+          </Link>
+          <h3 className='m-0'>COVID-19 CASE STATS</h3>
+          <span>
+            <FontAwesomeIcon icon={faMicrophone} className='me-4' />
+            <FontAwesomeIcon icon={faCog} />
+          </span>
+        </Navbar>
+      </header>
+      
+      <main>
+        <Routes>
+          <Route path='/details/:id' element={<Details />} />
+          <Route path='/' element={<HomePage />} />
+        </Routes>
+        </main>
       
     </div>
   );
